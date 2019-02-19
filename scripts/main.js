33541345cc0756ee5f3575bfdca1c1c81ed40cc6
hongngoctran20 <18040241@student.hhs.nl>
2019-02-19 13:27:05 +0100

    functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 //alert("Welcome on board!")
-
+var x = 35, y =50;
 //Gravity converter//
 document.getElementById("grvConvert").innerHTML =
-toGrav() + " g/l is equal to " + toGrav()/1000 + " kg/l";
+toGrav() + " g/l of density is equal to " + "<br>" + toGrav()/1000 + " sg of gravity";
 
 function toGrav(b) {
 	var b = 100;
@@ -19,7 +19,52 @@ function toMiles(a) {
 }
 
 //Date//
-var d = new Date();
-document.getElementById("date").innerHTML = d.toUTCString() + " Earth Time";
+var c = new Date();
+document.getElementById("date").innerHTML = c.toUTCString() + "+00 Earth Time";
 
 //Speed//
+document.getElementById("spd").innerHTML = spd() + " km/h";
+function spd(s) {
+	var s = x*y*10;
+	return s;
+}
+
+//gravity//
+document.getElementById("grv").innerHTML = grv();
+function grv(g) {
+	var g = x/y;
+	return g;
+}
+
+//fuel//
+document.getElementById("fuel").innerHTML = fuel() + " %";
+function fuel(f1) {
+	var f1 = x/y*100;
+	return f1;
+}
+
+//food//
+document.getElementById("food").innerHTML = food() + " %";
+function food(f2) {
+	var f2 = x+y-30;
+	return f2;
+}
+
+//distance//
+document.getElementById("distance").innerHTML = distance() + " km";
+function distance(d) {
+	var d = x*y*1234;
+	return d;
+}
+
+//water//
+document.getElementById("water").innerHTML =water() + " %";
+function water(w) {
+	var w = x+y-25;
+	return w;
+}
+	
+
+
+
+
