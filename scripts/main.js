0f08c2258d8a727e4f6f045eb1b56e9931dab6b8
hongngoctran20 <18040241@student.hhs.nl>
2019-02-19 13:59:48 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-//alert("Welcome on board!")
-var x = 35, y =50;
+alert("Welcome on board!")
+
 //Gravity converter//
 document.getElementById("grvConvert").innerHTML =
 toGrav() + " g/l of density is equal to " + "<br>" + toGrav()/1000 + " sg of gravity";
@@ -22,12 +22,21 @@ function toMiles(a) {
 var c = new Date();
 document.getElementById("date").innerHTML = c.toUTCString() + "+00 Earth Time";
 
+
+//Declare variables//
+var x = 35, y =50;
+
 //Speed//
 document.getElementById("spd").innerHTML = spd() + " km/h";
 function spd(s) {
-	var s = x*y*10;
+	var s = x*y*100;
 	return s;
 }
+if (spd()>200000) {
+	document.getElementById("spd").style.color = "red";
+} else if (spd()<=200000) {document.getElementById("spd").style.color = "orange";}
+else if (spd()<100000) {document.getElementById("spd").style.color = "green";}
+
 
 //gravity//
 document.getElementById("grv").innerHTML = grv();
@@ -35,6 +44,9 @@ function grv(g) {
 	var g = x/y;
 	return g;
 }
+if (grv()>=0.5) {
+	document.getElementById("grv").style.color = "lightgreen";
+} else {document.getElementById("grv").style.color = "red";}
 
 //fuel//
 document.getElementById("fuel").innerHTML = fuel() + " %";
@@ -42,6 +54,10 @@ function fuel(f1) {
 	var f1 = x/y*100;
 	return f1;
 }
+if (fuel()>65) {
+	document.getElementById("fuel").style.color = "lightgreen";
+} else if (fuel()>30) {document.getElementById("fuel").style.color = "orange";}
+else if (fuel()<=30) {document.getElementById("fuel").style.color = "red";}
 
 //food//
 document.getElementById("food").innerHTML = food() + " %";
@@ -49,6 +65,11 @@ function food(f2) {
 	var f2 = x+y-30;
 	return f2;
 }
+if (food()>60) {
+	document.getElementById("food").style.color = "lightgreen";
+} else if (food()>35) {document.getElementById("food").style.color = "orange";}
+else if (food()<=35) {document.getElementById("food").style.color = "red";}
+
 
 //distance//
 document.getElementById("distance").innerHTML = distance() + " km";
@@ -56,6 +77,11 @@ function distance(d) {
 	var d = x*y*12345;
 	return d;
 }
+if (distance()>2000000) {
+	document.getElementById("distance").style.color = "lightblue";
+} else if (distance()<=2000000) {document.getElementById("distance").style.color = "lightgreen";}
+else if (distance()<100000) {document.getElementById("distance").style.color = "yellow";}
+
 
 //water//
 document.getElementById("water").innerHTML =water() + " %";
@@ -63,7 +89,10 @@ function water(w) {
 	var w = x+y-25;
 	return w;
 }
-	
+if (water()>55) {
+	document.getElementById("water").style.color = "lightgreen";
+} else if (water()>35) {document.getElementById("water").style.color = "orange";}
+else if (water()<=35) {document.getElementById("water").style.color = "red";}
 
 
 
